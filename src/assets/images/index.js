d63261a45d31800f11105a63b342b7165cc3c17a
afifhibatullahh <afifhibatullah59@gmail.com>
2021-11-14 15:56:50 +0700

    add assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,27 @@
-import Logo from './logo.png'
-import IlustrasiRegister from './illustrationRegister.png'
+import Logo from './logo.png';
+import IlustrasiRegister from './illustrationRegister.png';
+import outletLogo from './outlet.png';
+import KeranjangIcon from './keranjang.png';
+import KeranjangIcon1 from './keranjang1.png';
+import iconRT from './icon-rt.png';
+import iconKonfirmasi from './IconKonfirmasi.png';
+import iconJam from './iconJam.png';
+import markIcon from './markIcon.png';
+import iconMotor from './motor.png';
+import iconMesinCuci from './mesinCuci.png';
+import iconMoney from './money.png';
 
-export {Logo, IlustrasiRegister};
+export {
+  Logo,
+  IlustrasiRegister,
+  outletLogo,
+  KeranjangIcon,
+  KeranjangIcon1,
+  iconJam,
+  iconRT,
+  iconMotor,
+  markIcon,
+  iconKonfirmasi,
+  iconMesinCuci,
+  iconMoney
+};
