60071c8dc87f898152b05a005c5b651992ba1f5c
afifhibatullahh <afifhibatullah59@gmail.com>
2021-12-05 14:40:14 +0700

    update ProfileUserSCreen

--- src/pages/User/Profil/index.js
+++ src/pages/User/Profil/index.js
@@ -1,12 +1,65 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {bgHeader} from '../../../assets/images';
+import SIZES from '../../../utils/constanta';
 
 const Profil = () => {
-    return(
-        <View>
-            <Text>Profil</Text>
+  return (
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <ImageBackground
+        source={bgHeader}
+        style={{width: SIZES.width, height: 120, paddingHorizontal: 20}}>
+        <Text
+          style={{
+            fontWeight: '700',
+            fontSize: 32,
+            color: 'white',
+            marginTop: 20,
+          }}>
+          Profil
+        </Text>
+      </ImageBackground>
+      <View style={{padding: 20}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Icon name="person" size={30} color="#C4C4C4" />
+          <Text
+            style={{
+              fontSize: 20,
+              color: 'black',
+              fontWeight: '700',
+              marginLeft: 10,
+            }}>
+            Tono
+          </Text>
         </View>
-    )
-}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Icon name="call" size={30} color="#C4C4C4" />
+          <Text
+            style={{
+              fontSize: 20,
+              color: 'black',
+              fontWeight: '700',
+              marginLeft: 10,
+            }}>
+            0812 3456 7890
+          </Text>
+        </View>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Icon name="mail" size={30} color="#C4C4C4" />
+          <Text
+            style={{
+              fontSize: 20,
+              color: 'black',
+              fontWeight: '700',
+              marginLeft: 10,
+            }}>
+            tono11@gmail.com
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export default Profil;
