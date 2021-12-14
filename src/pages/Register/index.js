37aab70ffac7324beaa5196579407f45228453b4
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-14 13:41:07 +0700

    merge page Register dan Profil user

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,29 +1,76 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, Image, ScrollView} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import SIZES, {ColorPrimary} from '../../utils/constanta';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Image, ScrollView, ToastAndroid } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import SIZES, { ColorPrimary } from '../../utils/constanta';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import {Fumi} from 'react-native-textinput-effects';
-import {IlustrasiRegister} from '../../assets/images';
+import { Fumi } from 'react-native-textinput-effects';
+import { IlustrasiRegister } from '../../assets/images';
 import { globalStyles } from '../../utils/global';
 
-const RegisterScreen = ({navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState('bal');
+  const [email, setEmail] = useState('bals@bal.com');
+  const [password, setPassword] = useState('1');
+  const [passwordConf, setPasswordConf] = useState('1');
+  const [phoneNumber, setPhoneNumber] = useState('123');
 
-  function register() {
-    navigation.replace('HomePage');
+  const registerPressed = async () => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (email && password && passwordConf && phoneNumber && name) {
+      if (emailRegex.test(email)) {
+        if (password == passwordConf) {
+          await fetch('http://192.168.42.63:8000/api/v1/register', {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              email: email,
+              password: password,
+              no_hp: phoneNumber,
+              name: name
+            }),
+          })
+            .then(response => response.json())
+            .then(responseJson => {
+              if (responseJson.errors == null) {
+                ToastAndroid.show('Berhasil membuat akun', ToastAndroid.SHORT);
+                navigation.navigate('LoginScreen');
+              } else {
+                alert('Email telah digunakan')
+              }
+            });
+        } else {
+          alert('Password tidak sama');
+        }
+      } else {
+        alert('Email tidak sesuai');
+      }
+    } else {
+      alert('Masukkan seluruh field');
+    }
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Daftar Akun</Text>
-      {/* <Image
-        source={IlustrasiRegister}
-        style={{width: 145, height: 166}}
-        resizeMode="center"
-      /> */}
+
+      <Fumi
+        label={'Nama'}
+        iconClass={FontAwesomeIcon}
+        iconName={'user'}
+        iconColor={ColorPrimary}
+        iconSize={20}
+        iconWidth={40}
+        inputPadding={20}
+        onChangeText={text => setName(text)}
+        autoCapitalize="none"
+        value={name}
+        style={styles.textInput}
+        inputStyle={globalStyles.bodyText}
+        labelStyle={globalStyles.captionText}
+      />
 
       <Fumi
         label={'Email'}
@@ -35,6 +82,7 @@ const RegisterScreen = ({navigation}) => {
         inputPadding={20}
         onChangeText={text => setEmail(text)}
         autoCapitalize="none"
+        keyboardType="email-address"
         value={email}
         style={styles.textInput}
         inputStyle={globalStyles.bodyText}
@@ -73,13 +121,29 @@ const RegisterScreen = ({navigation}) => {
         inputStyle={globalStyles.bodyText}
         labelStyle={globalStyles.captionText}
       />
+      <Fumi
+        label={'Konfirmasi Password'}
+        iconClass={FontAwesomeIcon}
+        iconName={'lock'}
+        iconColor={ColorPrimary}
+        iconSize={20}
+        iconWidth={40}
+        inputPadding={20}
+        onChangeText={text => setPasswordConf(text)}
+        autoCapitalize="none"
+        value={passwordConf}
+        style={styles.textInput}
+        secureTextEntry={true}
+        inputStyle={globalStyles.bodyText}
+        labelStyle={globalStyles.captionText}
+      />
 
       <View style={styles.bottom}>
-        <TouchableOpacity style={styles.button} onPress={() => register()}>
-          <Text style={{...globalStyles.titleText, color:'white'}}>Daftar</Text>
+        <TouchableOpacity style={styles.button} onPress={registerPressed}>
+          <Text style={{ ...globalStyles.titleText, color: 'white' }}>Daftar</Text>
         </TouchableOpacity>
         <View
-          style={{flexDirection: 'row', marginTop: 10, alignItems: 'center'}}>
+          style={{ flexDirection: 'row', marginTop: 10, alignItems: 'center' }}>
           <Text style={globalStyles.captionText}>Sudah Punya Akun?</Text>
           <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
             <Text
--- src/pages/User/Profil/index.js
+++ src/pages/User/Profil/index.js
@@ -1,59 +1,84 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ImageBackground } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {bgHeader} from '../../../assets/images';
+import { bgHeader } from '../../../assets/images';
 import SIZES from '../../../utils/constanta';
 import { globalStyles } from '../../../utils/global';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Loading } from '../../../components';
 
 const Profil = () => {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function getUser() {
+    if (name == '') {
+      const getUser = await AsyncStorage.getItem('user');
+      const parseObject = JSON.parse(getUser);
+      setName(parseObject.name);
+      setPhone(parseObject.no_hp);
+      setEmail(parseObject.email);
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    getUser()
+    setLoading(false)
+  }, [])
+
   return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
-      <ImageBackground
-        source={bgHeader}
-        style={{width: SIZES.width, height: 120, paddingHorizontal: 20}}>
-        <Text
-          style={{
-            fontFamily: 'Montserrat-Bold',
-            fontSize: 32,
-            color: 'white',
-            marginTop: 20,
-          }}>
-          Profil
-        </Text>
-      </ImageBackground>
-      <View style={{padding: 20}}>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Icon name="person" size={30} color="#C4C4C4" />
+    loading ? <Loading />
+      :
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <ImageBackground
+          source={bgHeader}
+          style={{ width: SIZES.width, height: 120, paddingHorizontal: 20 }}>
           <Text
             style={{
-              ...globalStyles.H3,
-              marginLeft: 10,
+              fontFamily: 'Montserrat-Bold',
+              fontSize: 32,
+              color: 'white',
+              marginTop: 20,
             }}>
-            Tono
+            Profil
           </Text>
-        </View>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Icon name="call" size={30} color="#C4C4C4" />
-          <Text
-            style={{
-              ...globalStyles.H3,
-              marginLeft: 10,
-            }}>
-            0812 3456 7890
-          </Text>
-        </View>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Icon name="mail" size={30} color="#C4C4C4" />
-          <Text
-            style={{
-              ...globalStyles.H3,
-              marginLeft: 10,
-            }}>
-            tono11@gmail.com
-          </Text>
+        </ImageBackground>
+        <View style={{ padding: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name="person" size={30} color="#C4C4C4" />
+            <Text
+              style={{
+                ...globalStyles.H3,
+                marginLeft: 10,
+              }}>
+              {name}
+            </Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+            <Icon name="call" size={30} color="#C4C4C4" />
+            <Text
+              style={{
+                ...globalStyles.H3,
+                marginLeft: 10,
+              }}>
+              {phone}
+            </Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+            <Icon name="mail" size={30} color="#C4C4C4" />
+            <Text
+              style={{
+                ...globalStyles.H3,
+                marginLeft: 10,
+              }}>
+              {email}
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
   );
 };
 
