ab032b37f501316376cb21443ba79798654f6f91
afifhibatullahh <afifhibatullah59@gmail.com>
2021-12-14 15:18:08 +0700

    fix profil user

--- src/pages/User/Profil/index.js
+++ src/pages/User/Profil/index.js
@@ -1,17 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { bgHeader } from '../../../assets/images';
-import SIZES from '../../../utils/constanta';
-import { globalStyles } from '../../../utils/global';
+import {bgHeader} from '../../../assets/images';
+import SIZES, { ColorPrimary } from '../../../utils/constanta';
+import {globalStyles} from '../../../utils/global';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Loading } from '../../../components';
+import {Loading} from '../../../components';
 
-const Profil = () => {
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [email, setEmail] = useState('')
-  const [loading, setLoading] = useState(false)
+const Profil = ({navigation}) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function getUser() {
     if (name == '') {
@@ -24,67 +30,82 @@ const Profil = () => {
   }
 
   useEffect(() => {
-    setLoading(true)
-    getUser()
-    setLoading(false)
-  }, [])
+    setLoading(true);
+    getUser();
+    setLoading(false);
+  }, []);
 
-  return (
-    loading ? <Loading />
-      :
-      (
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
-          <ImageBackground
-            source={bgHeader}
-            style={{ width: SIZES.width, height: 120, paddingHorizontal: 20 }}>
+  return loading ? (
+    <Loading />
+  ) : (
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <ImageBackground
+        source={bgHeader}
+        style={{width: SIZES.width, height: 120, paddingHorizontal: 20}}>
+        <Text
+          style={{
+            fontFamily: 'Montserrat-Bold',
+            fontSize: 32,
+            color: 'white',
+            marginTop: 20,
+          }}>
+          Profil
+        </Text>
+      </ImageBackground>
+      <View style={{padding: 20}}>
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent:'space-between'}}>
+          <View style={{flexDirection: 'row'}}>
+            <Icon name="person" size={30} color="#C4C4C4" />
             <Text
               style={{
-                fontFamily: 'Montserrat-Bold',
-                fontSize: 32,
-                color: 'white',
-                marginTop: 20,
+                ...globalStyles.H3,
+                marginLeft: 10,
               }}>
-              Profil
+              {name}
             </Text>
-          </ImageBackground>
-          <View style={{ padding: 20 }}>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Icon name="person" size={30} color="#C4C4C4" />
-              <Text
-                style={{
-                  ...globalStyles.H3,
-                  marginLeft: 10,
-                }}>
-                {name}
-              </Text>
-            </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-              <Icon name="call" size={30} color="#C4C4C4" />
-              <Text
-                style={{
-                  ...globalStyles.H3,
-                  marginLeft: 10,
-                }}>
-                {phone}
-              </Text>
-            </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-              <Icon name="mail" size={30} color="#C4C4C4" />
-              <Text
-                style={{
-                  ...globalStyles.H3,
-                  marginLeft: 10,
-                }}>
-                {email}
-              </Text>
-            </View>
           </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate('LoginScreen')}>
-            <Text style={{ ...globalStyles.H3, color: 'white' }}>Keluar</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('EditProfile')}>
+            <Icon
+              name="pencil"
+              size={24}
+              color="#fff"
+              style={{
+                padding: 8,
+                backgroundColor: ColorPrimary,
+                borderRadius: 30,
+              }}
+            />
           </TouchableOpacity>
-        </View>)
+        </View>
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 10}}>
+          <Icon name="call" size={30} color="#C4C4C4" />
+          <Text
+            style={{
+              ...globalStyles.H3,
+              marginLeft: 10,
+            }}>
+            {phone}
+          </Text>
+        </View>
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 10}}>
+          <Icon name="mail" size={30} color="#C4C4C4" />
+          <Text
+            style={{
+              ...globalStyles.H3,
+              marginLeft: 10,
+            }}>
+            {email}
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('LoginScreen')}>
+        <Text style={{...globalStyles.H3, color: 'white'}}>Keluar</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
